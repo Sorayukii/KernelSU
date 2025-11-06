ebbe4cf75a72b189d94743ba623e3d6bd67392b4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-06 11:39:59 +0800

    kernel: restore compat code required for old kernels
    This commit restores compatibility code needed that was removed at tiann/KernelSU@898e9d4 .
    where upstream dropped all pre-5.10 support
    
    Reverts `kernel:Add Huawei hisi check (tiann#1545)`
    	- upstream tiann/KernelSU@4f05fe2
    	- superceded by `kernel: expose allowlist workaround as Kconfig option` on next commit
    
    Reverts packages.list fsnotify watcher
    	- rename hook is way simpler and we have full access to LSM hooks on this kernel
    	- revert: cf031b482 - kernel: replace renameat hook with fsnotify
    	- revert: 5ac010d23 - kernel: fix compile
    	- revert: 3138651a3 - kernel: fix compile below 6.0
    
    Restores LSM hooks:
    	- inode_rename
    	- task_fix_setuid
    	- key permission
    
    Restores workqueue'd allowlist handling dropped at bfb767b, 9060e61
    Restores -Wno-implicit-function-declaration dropped at d3a09fa
    
    other changes and cleanups
    	sucompat: ksu_handle_stat(), remove dead ifdef.
    		- just use `ksu_handle_stat(&dfd, &filename->name, &flags);` if you want to hook vfs_statx on 6.1
    	LINUX_VERSION_CODE / KERNEL_VERSION, ksu.c
    		- reported by Sinclair19
    		- fix by including version.h
    	fatal_signal_pending, ksud.c
    		- add compat by including sched.h or sched/signal.h conditionally
    		- ref: https://github.com/torvalds/linux/commit/2a1f062a4acf0be50516ceece92a7182a173d55a
    	selinux_state.ss, core_hook.c
    		- remove rcu_dereference use
    		- ref: tiann#2695
    	seccomp.filter_count, core_hook.c
    		- reset this only for 5.9 and up as it only exists there
    		- ref: tiann#2708, https://github.com/gregkh/linux/commit/c818c03b661cd769e035e41673d5543ba2ebda64
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,6 +2,7 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 #include <linux/workqueue.h>
 
 #include "allowlist.h"
@@ -13,6 +14,13 @@
 #include "sucompat.h"
 #include "ksud.h"
 #include "supercalls.h"
+
+static struct workqueue_struct *ksu_workqueue;
+
+bool ksu_queue_work(struct work_struct *work)
+{
+	return queue_work(ksu_workqueue, work);
+}
 
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 					void *argv, void *envp, int *flags);
@@ -48,6 +56,8 @@ int __init kernelsu_init(void)
 
 	ksu_core_init();
 
+	ksu_workqueue = alloc_ordered_workqueue("kernelsu_work_queue", 0);
+
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
@@ -55,14 +65,13 @@ int __init kernelsu_init(void)
 	return 0;
 }
 
-extern void ksu_observer_exit(void);
 void kernelsu_exit(void)
 {
 	ksu_allowlist_exit();
 
 	ksu_throne_tracker_exit();
 
-	ksu_observer_exit();
+	destroy_workqueue(ksu_workqueue);
 
 	ksu_feature_exit();
 }
@@ -73,9 +82,10 @@ module_exit(kernelsu_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
 MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
-#else
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
 
